225d3f8dfafa339232d371bea9d082be5f1f80f3
namaneimages2026-dotcom <namaneimages.2026@gmail.com>
2026-04-25 21:14:53 +0200

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import LaserLab from './pages/LaserLab';
+
+export default function App() {
+  return (
+    <div style={{ background: '#000', color: '#fff', minHeight: '100vh' }}>
+      <h1 style={{ padding: 20 }}>Namane Supply</h1>
+      <LaserLab />
+    </div>
+  );
+}
